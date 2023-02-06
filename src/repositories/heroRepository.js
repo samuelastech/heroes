46f732ee8f42a24cc2521a29cf25d8b07e56bedc
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-02-06 11:57:47 -0300

    feat: heroRepository

---
+++ src/repositories/heroRepository.js
@@ -0,0 +1,26 @@
+const { readFile, writeFile } = require('fs/promises');
+
+class HeroRepository {
+  constructor({ file }) {
+    this.file = file;
+  }
+
+  async _currentFileContent() {
+    return JSON.parse(await readFile(this.file));
+  }
+
+  async find(itemId) {
+    const all = await this._currentFileContent();
+    if (!itemId) return all;
+    return all.find(({ id }) => itemId === id);
+  }
+
+  async create(data) {
+    const currentFile = await this._currentFileContent();
+    currentFile.push(data);
+    await writeFile(this.file, JSON.stringify(currentFile));
+    return data.id;
+  }
+}
+
+module.exports = HeroRepository;
