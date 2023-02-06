801dcfb9326e59e0a5c5e3a21b2228d5f33d4e5f
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-02-06 15:05:12 -0300

    feat: dependecy injection

---
+++ src/factories/heroFactory.js
@@ -0,0 +1,13 @@
+const HeroRepository = require('../repositories/heroRepository');
+const HeroService = require('../service/heroService');
+
+const { join } = require('path');
+const filename = join(__dirname, '../../database', 'data.json');
+
+const generateInstance = () => {
+  const heroRepository = new HeroRepository({ file: filename, });
+  const heroService = new HeroService({ heroRepository });
+  return heroService;
+};
+
+module.exports = { generateInstance };
